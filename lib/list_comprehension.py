ff65a22d3265019605d384af432ec6ae5d3a335a
Tejas Nampalli <TSNampalli@gmail.com>
2024-04-23 12:20:59 -0400

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    number_even = [num for num in num_list if num % 2 == 0]
+    return number_even
     pass
 
 def make_exclamation(sentence_list):
+    exclaimed_strings = [item + "!" for item in sentence_list]
+    return exclaimed_strings
     pass
